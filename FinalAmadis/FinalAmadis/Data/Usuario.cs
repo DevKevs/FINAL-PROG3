f26b9ac362b1b3b2fd769e0d6a0eff6551184bb0
kevin feliz <kevinfeliz2016@gmail.com>
2021-08-13 15:59:03 -0400

    First commit

---
+++ FinalAmadis/FinalAmadis/Data/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalAmadis.Data
+{
+    public class Usuario
+    {
+        public string Email { get; set; }
+        public string Passw { get; set; }
+        public string Rol { get; set; }
+    }
+}
