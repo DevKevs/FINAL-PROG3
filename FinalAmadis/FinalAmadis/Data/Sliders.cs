a39acc20b63e1a257fadef98ed393726ec22c15a
kevin feliz <kevinfeliz2016@gmail.com>
2021-08-17 17:37:44 -0400

    CRUD sliders

---
+++ FinalAmadis/FinalAmadis/Data/Sliders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalAmadis.Data
+{
+    public class Sliders
+    {
+        public int IdSlider { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio...")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio...")]
+        public string Recurso { get; set; }
+    }
+}
