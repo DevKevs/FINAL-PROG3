87b24c8935dcc6cd4f6153ab9dcb517df444ef9e
Eric Villanueva <20198493@itla.edu.do>
2021-08-19 20:24:29 -0400

    Carrito de compras Listo

---
+++ FinalAmadis/FinalAmadis/Data/AgregarProductos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalAmadis.Data
+{
+    public class AgregarProductos
+    {
+        public int IdUsuarios { get; set; }
+        public int IdProducto { get; set; }
+        public string Nombre_producto { get; set; }
+        public string Cantidad { get; set; }
+        public string Suma_total { get; set; }
+        public string Categoria { get; set; }
+        public string Descripcion { get; set; }
+        public string Foto { get; set; }
+        public string N_Stock { get; set; }
+    }
+}
